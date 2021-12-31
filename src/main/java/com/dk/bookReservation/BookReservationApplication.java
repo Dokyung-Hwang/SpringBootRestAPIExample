cfdf5ca56f22c149da4f7904f2691172cb2e2bdc
unknown <hdokyung94@gmail.com>
2021-12-31 18:57:58 +0900

    CREATE PROJECT

---
+++ src/main/java/com/dk/bookReservation/BookReservationApplication.java
@@ -0,0 +1,13 @@
+package com.dk.bookReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dk/bookReservation/BookReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.bookReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
